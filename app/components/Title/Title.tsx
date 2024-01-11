2cafd91c6c55968959162897edbea37173d1fbb7
Chaffexd <shanechaffe@gmail.com>
2024-01-11 21:44:23 +0000

    fix error

--- app/components/Title/Title.tsx
+++ app/components/Title/Title.tsx
@@ -1,9 +1,9 @@
 const Title = () => {
   return (
-    <>
+    <div>
       <h1 className="text-4xl font-bold text-center">Foodchat</h1>
       <p className="text-slate-400 text-center mt-2">Recipes made easy for those that don't know what to cook</p>
-    </>
+    </div>
   );
 };
 
